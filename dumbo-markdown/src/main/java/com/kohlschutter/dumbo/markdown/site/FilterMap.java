765e031bc0d933e15b60a684cd06dba4f2bcc6b3
Christian Kohlschütter <christian@kohlschutter.com>
2022-11-26 01:02:05 +0100

    dumbo-license: reformat, fix license header

--- dumbo-markdown/src/main/java/com/kohlschutter/dumbo/markdown/site/FilterMap.java
+++ dumbo-markdown/src/main/java/com/kohlschutter/dumbo/markdown/site/FilterMap.java
@@ -1,6 +1,7 @@
 /*
+ * dumbo-markdown
+ *
  * Copyright 2022 Christian Kohlschütter
- * Copyright 2014,2015 Evernote Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
