8f8a34796a27b019a06e3e4d6f33082dd507db8b
Christian Kohlschütter <christian@kohlschutter.com>
2025-04-06 19:58:05 +0200

    api: Make DumboTLSConfig final, add header

--- dumbo-api/src/main/java/com/kohlschutter/dumbo/api/DumboTLSConfig.java
+++ dumbo-api/src/main/java/com/kohlschutter/dumbo/api/DumboTLSConfig.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2022,2023 Christian Kohlschütter
+ * Copyright 2014,2015 Evernote Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kohlschutter.dumbo.api;
 
 import java.nio.file.Path;
 
-public class DumboTLSConfig {
+public final class DumboTLSConfig {
   private final String hostname;
   private final int port;
   private final Path keystore;
