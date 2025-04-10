85f72ddf9d3b2e65fe402cb986301821d0769906
Christian Kohlschütter <christian@kohlschutter.com>
2025-04-10 15:16:19 +0200

    api?: Add missing header

--- dumbo-api/src/main/java/com/kohlschutter/dumbo/annotations/ServletInitParameter.java
+++ dumbo-api/src/main/java/com/kohlschutter/dumbo/annotations/ServletInitParameter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022,2023 Christian Kohlschütter
+ * Copyright 2014,2015 Evernote Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kohlschutter.dumbo.annotations;
 
 import java.lang.annotation.ElementType;
@@ -24,7 +40,7 @@ public @interface ServletInitParameter {
    * <p>
    * By contract, the method must be idempotent. Moreover, there is no guarantee as to when the
    * method is called.
-   * 
+   *
    * @return The provider class, or {@code String.class} (the default) for "string as-is".
    */
   Class<?> valueProvider() default String.class;
